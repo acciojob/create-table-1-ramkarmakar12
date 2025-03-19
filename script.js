8169dfe80a6b90c2fd908b84204bb7659af5f5c9
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-19 08:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function insert_Row() {
+
     //Write your code here
-  
-  
+function insert_Row() {
+const table = document.getElementById("sampleTable");
+const newRow = table.insertRow(0);
+
+newRow.insertCell(0).textContent = "New Cell1";
+newRow.insertCell(1).textContent = "New Cell2";
 }
+
+
+
